a3ecd413d95ce86b97e3017cd01149f51f5a9636
Nutchy <nutchy@loomx.io>
2019-09-04 21:29:10 +0700

    Add card at function

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Printf("Result is : %s \n", cardAt(51))
+}
+
+// cardAt -
+func cardAt(n int) string {
+
+	if n < 0 || n > 51 {
+		return `Incorrect input number.`
+	}
+
+	// Declaration rank and suit of face card
+	rank := []string{"2", "3", "4", "5", "6", "7", "8", "9", "0", "J", "Q", "K", "A"}
+
+	// Club (♧), Diamond (♢), Heart (♡), Spade (♤)
+	suit := []string{"C", "D", "H", "S"}
+
+	// Use modulo and division to finding an index of rank & suit
+	return fmt.Sprintf("%s%s", rank[n%13], suit[n/13])
 }
